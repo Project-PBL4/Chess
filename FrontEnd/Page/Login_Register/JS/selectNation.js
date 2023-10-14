939ac1e71df47494bf98bf6a56b07c4b80a13144
DucVan <nguyenducvan260903@gmail.com>
2023-10-14 22:35:23 +0700

    [ADD] add event for chosing nation

---
+++ FrontEnd/Page/Login_Register/JS/selectNation.js
@@ -0,0 +1,17 @@
+function openAndCloseNation() {
+    const iconShow = cx(".icon-down");
+    const nationModal = cx(".select-nations");
+
+    if (nationModal.classList.contains("open")) {
+        nationModal.classList.remove("open");
+    } else {
+        nationModal.classList.add("open");
+    }
+}
+
+function selectNation(event) {
+    const inputNation = document.querySelector(".nation");
+    const nationModal = cx(".select-nations");
+    inputNation.value = event.target.getAttribute("value");
+    nationModal.classList.remove("open");
+}
