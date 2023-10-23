49f62f22104c0693359a99d3a4098b841c849942
DucVan <nguyenducvan260903@gmail.com>
2023-10-23 16:04:24 +0700

    [UPDATE] update some class name for suitable

--- FrontEnd/Page/Component/Heading/JS/openAndCloseNavBtn.js
+++ FrontEnd/Page/Component/Heading/JS/openAndCloseNavBtn.js
@@ -1,5 +1,5 @@
 function actionNavBtn() {
-    const listNav = window.parent.document.getElementById("list-option-sidebar");
+    const listNav = window.parent.document.getElementById("list-options-sidebar");
     if (listNav.style.display === "flex") {
         listNav.style.display = "none";
     } else {
