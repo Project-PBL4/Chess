a5086272ca6062eb00619df54387d380d48adcb3
DucVan <nguyenducvan260903@gmail.com>
2023-10-30 16:09:31 +0700

    [ADD] add extra screen when playing

--- FrontEnd/Page/Lobby/Body/Navigation/assets/JS/openRoomPlayWithAI.js
+++ FrontEnd/Page/Lobby/Body/Navigation/assets/JS/openRoomPlayWithAI.js
@@ -2,6 +2,10 @@ function openRoomWithAI() {
     const contentIframe = window.parent.document.getElementsByName("content")[0];
     const desc_img = contentIframe.contentDocument.getElementById("chessboard-desc_img");
     const roomAI = contentIframe.contentDocument.getElementById("chessboard");
+    const main_nav = document.querySelector(".main-nav");
+    const extra_playing_screen = document.querySelector(".extra-playing-screen");
+    main_nav.classList.remove("open");
+    extra_playing_screen.classList.add("open");
     roomAI.style.display = "flex";
     desc_img.style.display = "none";
 }
